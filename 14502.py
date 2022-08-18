afeff32c8723650471a28352f747a58ad533582b
Jinops <jinwoo.develop@gmail.com>
2022-08-19 02:06:12 +0900

    Update 14502.py
    Remove test code

--- 14502.py
+++ 14502.py
@@ -1,6 +1,5 @@
 import copy
 
-
 moveXY = [(1,0), (-1,0), (0,1), (0,-1)]
 n,m = map(int, input().split())
 lab = []
@@ -8,15 +7,6 @@ safeCount = 0
 
 for i in range(0,n):
     lab.append(list(map(int, input().split())))
-
-def test(lab, v):
-    print('#',v)
-    for x in range(n):
-        print(lab[x])
-    print('\n')
-
-def testWall(x,y,wall) :
-        print("@@", '\t'*wall,"(",wall,")", x, y)
 
 def spreadVirus(lab): # bfs
     q = []
@@ -26,7 +16,6 @@ def spreadVirus(lab): # bfs
                 q.append((x,y))
     
     while q:
-        print("@", q[0])
         currentX, currentY = q[0]
         for move in moveXY:
             x, y = currentX + move[0], currentY + move[1]
@@ -37,7 +26,6 @@ def spreadVirus(lab): # bfs
                 lab[x][y] = 2
                 q.append((x,y))
         q.pop(0)
-
 
 def getVirusedLab() :
     virusedLab = copy.deepcopy(lab)
@@ -59,20 +47,12 @@ def generateLabAndRun(moreWallCount):
         for x in range(n):
             for y in range(m):
                 if lab[x][y] == 0:
-                    #testWall(x,y,moreWallCount)
                     lab[x][y] = 1
                     generateLabAndRun(moreWallCount+1)
                     lab[x][y] = 0
     else :
-        test(lab, 'Before')
         virusedLab = getVirusedLab()
-        test(virusedLab, 'After')
         safeCount = max(safeCount, getSafeCount(virusedLab))
-        #test(1)
 
-#test(1)
 generateLabAndRun(0)
 print(safeCount)
-
-
-
