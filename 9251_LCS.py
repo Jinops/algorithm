bdfe4e6a1d691ff9ea6a03a56cd4a7978cd92004
jinops <jinwoo.develop@gmail.com>
2023-02-02 01:22:33 +0900

    Update 9251_LCS.py

--- 9251_LCS.py
+++ 9251_LCS.py
@@ -1,18 +1,16 @@
-aS = input("")
-bS = input("")
+# TODO
+# a = input()
+# b = input()
+a = "acaca"  
+b = "accca"
 
-max_len = 0
+cache = [[0]*(len(b)+1)]*(len(a)+1)
+for aIdx in range(len(a)):
+    for bIdx in range(len(b)):
+        if a[aIdx] == b[bIdx] :
+            cache[aIdx+1][bIdx+1] = cache[aIdx][bIdx] + 1
+        else :
+            cache[aIdx+1][bIdx+1] = max(cache[aIdx+1][bIdx], cache[aIdx][bIdx+1])
 
-for i in range(len(aS)):
-    LCS = ""
-    bIdx = 0
-    for aIdx in range(i, len(aS)):
-        bIdx = bS.find(aS[aIdx], bIdx)
-        if bIdx == -1:
-            continue
-        LCS += bS[bIdx]
-    
-    if len(LCS) > max_len:
-        max_len = len(LCS)
+print(cache[len(a)][len(b)])
 
-print(max_len)
