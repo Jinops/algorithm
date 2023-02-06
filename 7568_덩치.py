4d34d6914b6279706eb6f768d909dc134def43cf
jinops <jinwoo.develop@gmail.com>
2023-02-06 22:21:11 +0900

    Update 7568_덩치.py

--- 7568_덩치.py
+++ 7568_덩치.py
@@ -1,26 +1,15 @@
-# TODO
 n = int(input())
 data_list = []
-rank_list = [n] * n
+rank_list = [1] * n
 
-for rank in range(n):
+for _ in range(n):
     kg, cm = map(int,input().split())
     data_list.append((kg, cm))
 
-for rank in range(n):
-    for j in range(rank+1, n):
-        if data_list[rank][0] > data_list[j][0] and data_list[rank][1] > data_list[j][1]:
-            rank_list[rank] -= 1
+for i in range(n):
+    for j in range(n):
+        if data_list[i][0] < data_list[j][0] \
+        and data_list[i][1] < data_list[j][1]:
+            rank_list[i] += 1
 
-diff = min(rank_list)-1
-diff_sub = 0
-diff_dict = {}
-for rank in sorted(rank_list):
-    if rank not in diff_dict:
-        diff_dict[rank] = diff + diff_sub
-        diff_sub = 0
-    else:
-        diff_sub -= 1
-
-rank_list = list(map(lambda rank: str(rank - diff_dict[rank]), rank_list))
-print(' '.join(rank_list))
+print(' '.join(map(str, rank_list)))
