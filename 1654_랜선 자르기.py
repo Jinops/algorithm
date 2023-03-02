07d0dd35690e4d6e990a73118712a75a250e6f23
jinops <jinwoo.develop@gmail.com>
2023-03-02 11:11:53 +0900

    Update 1654_랜선 자르기.py

--- 1654_랜선 자르기.py
+++ 1654_랜선 자르기.py
@@ -1,23 +1,14 @@
 def b_search(start, end):
     if start > end:
-        return 0
+        return end
     mid = (start+end)//2
     count = get_count(mid)
     global m
 
-    if count == m:
-        return s_search(mid, end)
-    elif count > m:
+    if count >= m:
         return b_search(mid+1, end)
     else:
         return b_search(start, mid-1)
-    
-def s_search(start, end):
-    global m
-    while start <= end \
-        and get_count(start) == m:
-        start += 1
-    return start -1
         
 def get_count(length):
     global datas
@@ -31,7 +22,7 @@ datas = []
 for _ in range(k):
     datas.append(int(input()))
 
-start = max(datas) // m
+start = max(max(datas) // m, 1)
 end = sum(datas) // m
 
 print(b_search(start, end))
