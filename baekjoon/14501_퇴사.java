fe0985d90e3fa614e39c6b9392fd9f0ee8f8d68c
Jinwoo Park <jinwoo.develop@gmail.com>
2024-01-19 17:39:32 +0900

    Update 14501_퇴사.java

--- baekjoon/14501_퇴사.java
+++ baekjoon/14501_퇴사.java
@@ -12,7 +12,7 @@ public class Main {
 		int[] pays = new int[lastDay+1];
 		int[][] dp = new int[lastDay+1][lastDay+1];
 		int result = 0;
-		// 최대이익 = dp[n일차][n일 여유 있을 때]
+		// 최대이익 = dp[i일차][j일차 일 했을 때]
 		
 		String a =new String("banana");
 		StringTokenizer st;
@@ -22,17 +22,13 @@ public class Main {
 			days[i] = Integer.parseInt(st.nextToken());
 			pays[i] = Integer.parseInt(st.nextToken());
 		}
-
+		
 		for(int i=1; i<lastDay; i++) {
 			for(int j=1; j<=lastDay; j++) {
-				if(j >=days[i]) {
-					if(i+days[i]<=lastDay+1) {
-						dp[i][j] += pays[i];
-						dp[i+1][j-days[i]] += dp[i][j];
-					} else {
-						dp[i+1][j] += dp[i][j];
-					} 
+				if(i>=j+days[j]-1) {
+					dp[i][j] += pays[j];
 				}
+				dp[i+1][j]=dp[i][j];
 			}
 		}
 		// DEBUG
@@ -43,6 +39,7 @@ public class Main {
 			}
 			System.out.println();
 		}
+		//
 		//
 		System.out.println(result);
 	}
