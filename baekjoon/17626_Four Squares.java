17013a7f88befd0e5d8c01d98c67246839ee702c
Jinwoo Park <jinwoo.develop@gmail.com>
2024-01-23 15:43:22 +0900

    Update 17626_Four Squares.java

--- baekjoon/17626_Four Squares.java
+++ baekjoon/17626_Four Squares.java
@@ -14,16 +14,38 @@ public class Main {
 			return;
 		}
 		int[][] dp = new int[max+1][2];
+		int[][] dpCount = new int[max+1][2];
 		dp[max][1] = max*max;
+		dp[max][1] = 1;
+		System.out.println(max+Arrays.toString(dp[max]));
 		
 		for(int i=max-1; i>0; i--) {
 			dp[i][0] = Math.max(dp[i+1][0], dp[i+1][1]);
+			int temp1 = 0;
+			int temp2 = 0;
+			
 			if(dp[i+1][1]+Math.pow(i, 2)<=n) {
-				dp[i][1] = (int) (dp[i+1][1]+Math.pow(i, 2));
-			} if(dp[i+1][0]+Math.pow(i, 2)<=n) {
-				dp[i][1] = Math.max(dp[i][1], (int) (dp[i+1][0]+Math.pow(i, 2)));
+				temp1 = (int) (dp[i+1][1]+Math.pow(i, 2));
+			} 
+			if(dp[i+1][0]+Math.pow(i, 2)<=n) {
+				temp2 = (int) (dp[i+1][0]+Math.pow(i, 2));
 			}
-			System.out.println(Arrays.toString(dp[i]));
+			
+			if(temp1 > temp2) {
+				dp[i][1] = temp1;
+				dpCount[i][1] += dpCount[i+1][1]+1;
+			} else if (temp1 < temp2) {
+				dp[i][1] = temp2;
+				dpCount[i][1] += dpCount[i+1][0]+1;
+			} else if (dpCount[i+1][1]>=dpCount[i+1][0]) {
+				dp[i][1] = temp1;
+				dpCount[i][1] += dpCount[i+1][1]+1;
+			} else {
+				dp[i][1] = temp2;
+				dpCount[i][1] += dpCount[i+1][0]+1;
+			}
+			
+			System.out.println(i+Arrays.toString(dp[i]));
 		}
 		
 	}
