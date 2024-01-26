5dcb804b63c9460164859f38395df64abc2c73da
Jinwoo Park <jinwoo.develop@gmail.com>
2024-01-26 17:28:07 +0900

    Update 1260_DFS와 BFS.java

--- baekjoon/1260_DFS와 BFS.java
+++ baekjoon/1260_DFS와 BFS.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -14,47 +13,59 @@ public class Main {
   static int v;
   static HashMap<Integer, ArrayList<Integer>> edges = new HashMap<>();
   
-  static void dfs(int start, boolean visited[], Queue<Integer> route) {
+  static void printRoute(LinkedList<Integer> route) {
+    for(int node:route) {
+      System.out.print(node+" ");
+    }
+    System.out.println();
+  }
+  
+  static void dfs(int start, boolean visited[], LinkedList<Integer> route) {
     route.add(start);
     visited[start] = true;
+    route.add(start);
     if(route.size() == n) {
-      for(int node:route) {
-        System.out.print(node+" ");
-      }
-      System.out.println();
       return;
     }
-    if(edges.get(start)==null) {
-      return;
-    }
-    for(int node:edges.get(start)) {
-      if(!visited[node]) {
-        dfs(node, visited, route);
+    if(edges.get(start)!=null) {
+      for(int node:edges.get(start)) {
+        if(!visited[node]) { // TODO
+          dfs(node, visited, route);
+        }
       }
     }
   }
   
-  static void bfs() {
-    String result = "";
+  static LinkedList<Integer> bfs() {
     Queue<Integer> queue = new LinkedList<>();
+    LinkedList<Integer> route = new LinkedList<>();
     boolean[] visited = new boolean[n+1];
     queue.add(v);
+    route.add(v);
     visited[v] = true;
     
     while(!queue.isEmpty()) {
       int start = queue.poll();
-      result += start + " ";
       if(edges.get(start)!=null) {
         for(int node:edges.get(start)) {
           if(!visited[node]) {
             visited[node] = true;
             queue.add(node);
+            route.add(node);
           }
         }
       }
     }
     
-    System.out.println(result);
+    if(route.size() < n) {
+      for(int i=1; i<=n; i++) {
+        if(!route.contains(i)) {
+          route.add(i);
+        }
+      }
+    }
+    
+    return route;
   }
   
   public static void main(String[] args) throws IOException {
@@ -79,6 +90,7 @@ public class Main {
     }
     
     dfs(v, new boolean[n+1], new LinkedList<>());
-    bfs();
+    System.err.println("---");
+    printRoute(bfs());
   }
 }
