95b955b70e167c6ccc4b0d01757e423192d2e865
Jinwoo Park <jinwoo.develop@gmail.com>
2024-01-30 10:26:46 +0900

    Update 30014_준영이의 사랑.java

--- baekjoon/30014_준영이의 사랑.java
+++ baekjoon/30014_준영이의 사랑.java
@@ -2,33 +2,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
   static int N;
   static int[] arr;
   static int bestResult;
-  static LinkedList<Integer> bestStack;
+  static LinkedList<Integer> bestResutList;
   
-  static int calculate(Stack<Integer> stack) {
-    int result = stack.lastElement() * stack.firstElement();
-    for(int i=0; i<stack.size()-1; i++) {
-      result += stack.get(i) * stack.get(i+1);
+  static int calculate(LinkedList<Integer> list) {
+    int result = list.get(0) * list.get(N-1);
+    for(int i=0; i<N-1; i++) {
+      result += list.get(i) * list.get(i+1);
     }
     return result;
   }
   
-  static void run(int multiplied, LinkedList<Integer> list) {
-    if(list.size() == N && multiplied > bestResult) {
-//      System.out.println(list.toString());
-      bestResult = multiplied;
-      bestStack = list;
+  static void run(LinkedList<Integer> list) {
+    if(list.size() == N) {
+      int result = calculate(list);
+      if(result > bestResult) {
+        bestResult = result;
+        bestResutList = (LinkedList<Integer>) list.clone();
+      }
     }
     for(int n:arr) {
       if(!list.contains(n)) {
         list.add(n);
-        run(multiplied*n, list);
+        run(list);
         list.pop();
       }
     }
@@ -44,11 +45,8 @@ public class Main {
     for(int i=0; i<N; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
     }
-    long beforeTime = System.currentTimeMillis();
-    run(1, new LinkedList<Integer>());
-    System.out.println(bestResult);
-    System.out.println(bestStack.toString().replace("[", "").replace("]", "").replaceAll(",", ""));
-
-    System.out.println("시간차이(m) : "+( System.currentTimeMillis() - beforeTime));
+    
+    run(new LinkedList<Integer>());
+    System.out.println(bestResult);System.out.println(bestResutList.toString().replace("[", "").replace("]", "").replaceAll(",", ""));
   }
 }
