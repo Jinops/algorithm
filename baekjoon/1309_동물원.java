fbac4a57ee581d0d9142aa3452d73959591ffc37
Jinops <jinwoo.develop@gmail.com>
2024-02-02 23:54:18 +0900

    Update 1309_동물원.java

--- baekjoon/1309_동물원.java
+++ baekjoon/1309_동물원.java
@@ -1,58 +1,19 @@
 import java.util.*;
 
 public class Main {
-	static int N;
-	static int result = 0;
-	
-	static boolean isValid(boolean[][] m, int i, int j) {
-		if(m[i][j]) {
-			return false;
-		}
-		if(i>0 && m[i-1][j] || i<N-1 && m[i+1][j]) { // 위아래에 true 있으면
-			return false;
-		}
-		if(j>0 && m[i][j-1] || j<N-1 && m[i][j+1]) { // 좌우에 true 있으면
-			return false;
-		}
-		return true;
-	}
-	
-	static void set(boolean[][] m, int lastI, int lastJ, int cnt) {
-		if(cnt == N) {
-			result += 1;
-
-			for(boolean[] b:m) {
-				System.out.println(Arrays.toString(b));
-			}
-			System.out.println();
-			return;
-		}
-		// TODO: last i & j
-		int startI = lastI+1;
-		int startJ = lastJ+1;
-		if(startJ==N) {
-			startI += 1;
-			startJ = 0;
-		}
-		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				// TODO
-				if(isValid(m, i, j)) {
-					m[i][j] = true;
-					set(m, i, j, cnt+1); // TODO: y,x
-					m[i][j] = false;
-				}
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		boolean[][] m = new boolean[N][N]; // true일 때 배치불가
-
-		set(m, 0, -1, 0);
-		System.out.println(result);
-	}
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int dp[][] = new int[N+1][3]; // 0:x ,1:왼쪽, 2:오른쪽
+    
+    dp[0][0] = 1;
+    
+    for(int i=0; i<N; i++) {
+      dp[i+1][0] = (dp[i][0] + dp[i][1] + dp[i][1]) % 9901;
+      dp[i+1][1] = (dp[i][0] + dp[i][2]) % 9901;
+      dp[i+1][2] = (dp[i][0] + dp[i][1]) % 9901;
+    }
+    
+    System.out.println((dp[N][0]+dp[N][1]+dp[N][2]) % 9901);
+  }
 }
