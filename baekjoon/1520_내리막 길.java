cf1f25e5d776ad6eca0dc90b8ac7e9befe65b4cc
Jinwoo Park <jinwoo.develop@gmail.com>
2024-02-06 16:30:26 +0900

    1520_메모리 초과 수정
    메모리 초과 해결

--- baekjoon/1520_내리막 길.java
+++ baekjoon/1520_내리막 길.java
@@ -1,54 +1,30 @@
-// 13% 메모리 초과
 import java.util.*;
-import java.awt.Point;
 import java.io.*;
 
 public class Main {
   static int M, N;
   static int[][] map;
-  static boolean[][] isToEnd;
-  static int result  = 0;
-  
+  static int[][] dp; // -1:방문x, 0:방문o, 1이상:해당 경로 통해 최종 갈 수 있는 경우
   static final int[][] deltas = {{1,0},{-1,0},{0,1},{0,-1}};
   
-  static Point[] getNearPoints(int x, int y) {
-    ArrayList<Point> points = new ArrayList<>();
+  static int dfs(int x, int y) {
+    if(dp[y][x]!=-1) {
+      return dp[y][x];
+    }
+    if(x==N-1 && y==M-1) {
+      return 1;
+    }
+    dp[y][x]=0;
+    
     for(int[] delta:deltas) {
       int nx = x+delta[0];
       int ny = y+delta[1];
       if(0<=nx && nx<N && 0<=ny && ny<M && map[y][x]>map[ny][nx]) {
-          points.add(new Point(nx, ny));
+          dp[y][x] += dfs(nx, ny);
         }
       }
-    return points.toArray(new Point[points.size()]);
-  }
-  
-  static void setIsToEnd(Queue<Point> queue) {
-    for(Point np:queue) {
-      isToEnd[np.y][np.x] = true; 
-    }
-  }
-  
-  static void DFS(Deque<Point> queue) {
-    Point p = queue.peekLast();
-//    System.out.println(queue.toString());
-    if(isToEnd[p.y][p.x] || (p.x==N-1 && p.y==M-1)) {
-      result += 1;
-      setIsToEnd(queue);
-      return;
-    }
     
-    Point[] nearPoints = getNearPoints(p.x, p.y);
-    for(Point np:nearPoints) {
-      if(nearPoints.length == 1) {
-        queue.add(np);
-        DFS(queue);
-      } else { // 분기 시 분기 이후의 것들을 따로 queue에 넣음
-        Deque<Point> branchedQueue = new LinkedList<>();
-        branchedQueue.add(np);
-        DFS(branchedQueue);
-      }
-    }
+    return dp[y][x]; // 더이상 가 곳 없을 때, 현재 dp값 출력. (최종적으론 0,0)
   }
   
   public static void main(String[] args) throws IOException {
@@ -58,21 +34,16 @@ public class Main {
     M = Integer.parseInt(st.nextToken());
     N = Integer.parseInt(st.nextToken());
     map = new int[M][N];
-    isToEnd = new boolean[M][N];
+    dp = new int[M][N];
     
     for(int i=0; i<M; i++) {
       st = new StringTokenizer(br.readLine());
       for(int j=0; j<N; j++) {
         map[i][j] = Integer.parseInt(st.nextToken());
+        dp[i][j] = -1;
       }
     };
     
-    Deque<Point> queue = new LinkedList<>();
-    queue.add(new Point(0, 0));
-    
-    DFS(queue);
-    
-    
-    System.out.println(result);
+    System.out.println(dfs(0 ,0));
   }
 }
