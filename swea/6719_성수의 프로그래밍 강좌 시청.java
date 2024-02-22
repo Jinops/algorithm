83d7c4fc627bbb11901b013151f82a01a6f4d7c3
Jinops <jinwoo.develop@gmail.com>
2024-02-23 00:44:14 +0900

    Update 6719_성수의 프로그래밍 강좌 시청.java

--- swea/6719_성수의 프로그래밍 강좌 시청.java
+++ swea/6719_성수의 프로그래밍 강좌 시청.java
@@ -2,25 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-  static int N;
-  static int K;
-  static int nums[];
-  static double max;
-  
-  public static void pick(int idx, double A, Stack<Integer> picked) {
-    if (picked.size()==K) {
-      max = Math.max(max, A);
-      return;
-    }
-    for(int i=0; i<N; i++) {
-      if(!picked.contains(i)) {
-        picked.add(i);
-        pick(i+1, (A+nums[i])/2.0, picked);
-        picked.pop();
-      }
-    }
-  }
-
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st;
@@ -29,20 +10,21 @@ public class Solution {
     for (int t=1; t<=T; t++) {
 
       st = new StringTokenizer(br.readLine());
-      N = Integer.parseInt(st.nextToken());
-      K = Integer.parseInt(st.nextToken());
-      nums = new int[N];
-      max = Integer.MIN_VALUE;
-      Stack<Integer> picked = new Stack<>();
+      int N = Integer.parseInt(st.nextToken());
+      int K = Integer.parseInt(st.nextToken());
+      ArrayList<Integer> arr = new ArrayList<>();
       
       st = new StringTokenizer(br.readLine());
       for(int i=0; i<N; i++) {
-        nums[i] = Integer.parseInt(st.nextToken());
+        arr.add(Integer.parseInt(st.nextToken()));
       }
-      
-      pick(0, 0, picked);
+      arr.sort(null);
+      double result = 0;
+      for(int i=N-K; i<N; i++) {
+        result = (result+arr.get(i))/2;
+      }
 
-      System.out.printf("#%d %f\n", t, max);
+      System.out.printf("#%d %f\n", t, result);
     }
   }
 }
