2b47eda70fb0364e2dfba04b500d9eaa73d54018
Jinwoo Park <jinwoo.develop@gmail.com>
2024-04-24 17:48:56 +0900

    Update 1240_노드사이의 거리.java

--- baekjoon/1240_노드사이의 거리.java
+++ baekjoon/1240_노드사이의 거리.java
@@ -1,5 +1,3 @@
-// 다익스트라 풀이
-// 트리이므로, 다익스트라 풀이보다는 BFS가 적절할듯 (거리가 갱신되지 않기 때문)
 import java.util.*;
 import java.io.*;
 
@@ -45,37 +43,30 @@ public class Main {
       st = new StringTokenizer(br.readLine());
       int from = Integer.parseInt(st.nextToken());
       int to = Integer.parseInt(st.nextToken());
-      // from으로부터 다익스트라 수행
       
-      int[] dists = new int[N+1];
-      Arrays.fill(dists, Integer.MAX_VALUE);
+      int result = -1;
       
       PriorityQueue<Node> pq = new PriorityQueue<>();
-      boolean[] confirmed = new boolean[N+1];
+      boolean[] visited = new boolean[N+1];
       
       pq.add(new Node(from, 0));
-      dists[from] = 0;
+      visited[from] = true;
       
-      while(!pq.isEmpty() && !confirmed[to]) {
+      while(!pq.isEmpty()) {
         Node node = pq.poll();
-        
-        if(confirmed[node.n]) {
-          continue;
+        if(node.n == to) {
+          result = node.dist;
+          break;
         }
-        confirmed[node.n]= true;
+        
         for(Node next:nodes[node.n]) {
-          if(confirmed[next.n]) continue;
-          
-          int dist = dists[node.n] + next.dist;
-          // (from->n) 거리 + (n->next) 거리
-          if(dist < dists[next.n]) {
-            // ... < (from-> next) 거리
-            dists[next.n] = dist;
-            pq.add(new Node(next.n, dists[next.n]));
-          }
+          if(visited[next.n]) continue;
+          visited[next.n] = true;
+          pq.add(new Node(next.n, node.dist+next.dist));
         }
       }
-      System.out.println(dists[to]);
+      
+      System.out.println(result);
     }
   }
 }
