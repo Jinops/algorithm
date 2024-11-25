84d6cfaec2947b573956b1195e473268eba943dd
Jinops <jinwoo.develop@gmail.com>
2024-11-25 19:55:23 +0900

    Update 2638_치즈.py

--- baekjoon/2638_치즈.py
+++ baekjoon/2638_치즈.py
@@ -2,18 +2,26 @@ from collections import deque
 
 ds = ((1,0), (-1,0), (0,1), (0,-1))
 
-n, m = map(int, input().split())
-mtx = [list(map(int, input().split())) for _ in range(n)]
+N, M = map(int, input().split())
+mtx = [list(map(int, input().split())) for _ in range(N)]
 
 def in_range(y, x):
-  return 0<=y<n and 0<=x<m
+  return 0<=y<N and 0<=x<M
 
+def is_cheese_left():
+  for y in range(N):
+    for x in range(M):
+      if mtx[y][x] == 1:
+        return True
+  return False
 
-while True:
+time = 0
+
+while is_cheese_left():
   queue = deque()
   queue.append((0, 0))
-  visited = [[False] * m for _ in range(n)]
-  touched = [[False] * m for _ in range(n)]
+  visited = [[False] * M for _ in range(N)]
+  touch_cnt = [[0] * M for _ in range(N)]
 
   while queue:
     y, x = queue.popleft()
@@ -23,6 +31,18 @@ while True:
       if not in_range(ny, nx):
         continue
       
-      if not visited[ny][nx] and mtx[ny][nx] == 0:
+      if mtx[ny][nx] == 1:
+        touch_cnt[ny][nx] += 1
+
+      elif not visited[ny][nx] and mtx[ny][nx] == 0:
         visited[ny][nx] = True
         queue.append((ny, nx))
+  
+  for y in range(N):
+    for x in range(M):
+      if touch_cnt[y][x] >= 2:
+        mtx[y][x] = 0
+  
+  time += 1
+
+print(time)
